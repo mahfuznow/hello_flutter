c745f94d32717c2a546585889e9a682e93c10fef
Md. Mahfuzur Rahman <mahfuzur.rahman@brainstation-23.com>
2024-06-07 18:02:10 +0600

    Improved NetworkImageView

--- lib/common/widget/network_image_view.dart
+++ lib/common/widget/network_image_view.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hello_flutter/values/dimens.dart';
 
 class NetworkImageView extends StatelessWidget {
   final String url;
-  final Widget placeholder;
+  final Widget? placeholder;
   final double height;
   final double width;
   final BoxFit? fit;
@@ -10,7 +11,7 @@ class NetworkImageView extends StatelessWidget {
 
   const NetworkImageView({
     required this.url,
-    this.placeholder = const CircularProgressIndicator(),
+    this.placeholder,
     this.height = double.infinity,
     this.width = double.infinity,
     this.fit,
@@ -28,9 +29,16 @@ class NetworkImageView extends StatelessWidget {
         height: height,
         width: width,
         frameBuilder: (context, child, frame, wasSynchronouslyLoaded) {
-          return frame == null ? placeholder : child;
+          return frame == null ? placeholder ?? _placeholder() : child;
         },
       ),
+    );
+  }
+
+  Widget _placeholder() {
+    return Padding(
+      padding: EdgeInsets.all(Dimens.dimen_8),
+      child: const CircularProgressIndicator(),
     );
   }
 }
