91aaf4afd8e97756347875737a06ba3db89e5bcf
Md. Mahfuzur Rahman <mahfuzur.rahman@brainstation-23.com>
2024-07-11 16:41:04 +0600

    Added generated file contents.

--- lib/presentation/feature/create_feature.dart
+++ lib/presentation/feature/create_feature.dart
@@ -21,70 +21,197 @@ void createFeatureStructure(String featureName) {
 
   final files = {
     '$featurePath/binding/${featureName}_binding.dart':
-        bindingTemplate(featureName),
+        bindingContent(featureName),
     '$featurePath/route/${featureName}_argument.dart':
-        argumentTemplate(featureName),
-    '$featurePath/route/${featureName}_route.dart': routeTemplate(featureName),
+        argumentContent(featureName),
+    '$featurePath/route/${featureName}_route.dart': routeContent(featureName),
     '$featurePath/screen/${featureName}_mobile_portrait.dart':
-        mobilePortraitTemplate(featureName),
+        mobilePortraitContent(featureName),
     '$featurePath/screen/${featureName}_mobile_landscape.dart':
-        mobileLandscapeTemplate(featureName),
+        mobileLandscapeContent(featureName),
     '$featurePath/${featureName}_adaptive_ui.dart':
-        adaptiveUiTemplate(featureName),
+        adaptiveUiContent(featureName),
     '$featurePath/${featureName}_view_model.dart':
-        viewModelTemplate(featureName),
+        viewModelContent(featureName),
   };
 
   for (final path in paths) {
     Directory(path).createSync(recursive: true);
   }
 
-  for (final file in files.entries) {
-    File(file.key).writeAsStringSync(file.value);
-  }
+  files.forEach((filePath, fileContent) {
+    File(filePath).writeAsStringSync(fileContent);
+  });
 
   print('Feature "$featureName" structure created successfully.');
 }
 
-String bindingTemplate(String featureName) => '''
-class ${featureName.capitalize()}Binding {
-  // TODO: Add binding code here
+String bindingContent(String featureName) => '''
+import 'package:hello_flutter/presentation/base/base_binding.dart';
+
+class ${featureName.capitalize()}Binding extends BaseBinding {
+  @override
+  Future<void> addDependencies() async {
+    // MovieRepository movieRepository = await diModule.resolve<MovieRepository>();
+    // return diModule.registerInstance(
+    //   ${featureName.capitalize()}ViewModel(movieRepository: movieRepository),
+    // );
+  }
+
+  @override
+  Future<void> removeDependencies() async {
+    // return diModule.unregister<${featureName.capitalize()}ViewModel>();
+  }
 }
 ''';
 
-String argumentTemplate(String featureName) => '''
-class ${featureName.capitalize()}Argument {
-  // TODO: Add argument code here
+String argumentContent(String featureName) => '''
+import 'package:hello_flutter/presentation/base/base_argument.dart';
+
+class ${featureName.capitalize()}Argument extends BaseArgument {}
+''';
+
+String routeContent(String featureName) => '''
+import 'package:flutter/material.dart';
+import 'package:hello_flutter/presentation/base/base_route.dart';
+import 'package:hello_flutter/presentation/navigation/route_path.dart';
+import '../${featureName}_adaptive_ui.dart';
+import '../route/${featureName}_argument.dart';
+
+class ${featureName.capitalize()}Route extends BaseRoute<${featureName.capitalize()}Argument> {
+  @override
+  RoutePath routePath = RoutePath.$featureName;
+
+  ${featureName.capitalize()}Route({required super.arguments});
+
+  @override
+  MaterialPageRoute toMaterialPageRoute() {
+    return MaterialPageRoute(builder: (_) => const ${featureName.capitalize()}AdaptiveUi());
+  }
 }
 ''';
 
-String routeTemplate(String featureName) => '''
-class ${featureName.capitalize()}Route {
-  // TODO: Add route code here
+String mobilePortraitContent(String featureName) => '''
+import 'package:flutter/material.dart';
+import 'package:hello_flutter/presentation/base/base_ui_state.dart';
+import '../${featureName}_view_model.dart';
+
+class ${featureName.capitalize()}MobilePortrait extends StatefulWidget {
+  final ${featureName.capitalize()}ViewModel viewModel;
+
+  const ${featureName.capitalize()}MobilePortrait({required this.viewModel, super.key});
+
+  @override
+  State<StatefulWidget> createState() => ${featureName.capitalize()}MobilePortraitState();
+}
+
+class ${featureName.capitalize()}MobilePortraitState extends BaseUiState<${featureName.capitalize()}MobilePortrait> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: valueListenableBuilder(
+        listenable: widget.viewModel.message,
+        builder: (context, value) {
+          return InkWell(
+            child: Text('Dashboard: \$value'),
+            onTap: () => widget.viewModel.onClick(),
+          );
+        },
+      ),
+    );
+  }
 }
 ''';
 
-String mobilePortraitTemplate(String featureName) => '''
-class ${featureName.capitalize()}MobilePortrait {
-  // TODO: Add mobile portrait code here
+String mobileLandscapeContent(String featureName) => '''
+import 'package:flutter/material.dart';
+import '../screen/${featureName}_mobile_portrait.dart';
+
+class ${featureName.capitalize()}MobileLandscape extends ${featureName.capitalize()}MobilePortrait {
+  const ${featureName.capitalize()}MobileLandscape({required super.viewModel, super.key});
+
+  @override
+  State<StatefulWidget> createState() => ${featureName.capitalize()}MobileLandscapeState();
+}
+
+class ${featureName.capitalize()}MobileLandscapeState extends ${featureName.capitalize()}MobilePortraitState {
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+      body: valueListenableBuilder(
+        listenable: widget.viewModel.message,
+        builder: (context, value) {
+          return InkWell(
+            child: Text('Dashboard: \$value'),
+            onTap: () => widget.viewModel.onClick(),
+          );
+        },
+      ),
+    );
+  }
 }
 ''';
 
-String mobileLandscapeTemplate(String featureName) => '''
-class ${featureName.capitalize()}MobileLandscape {
-  // TODO: Add mobile landscape code here
+String adaptiveUiContent(String featureName) => '''
+import 'package:flutter/material.dart';
+import 'package:hello_flutter/presentation/base/base_adaptive_ui.dart';
+import './binding/${featureName}_binding.dart';
+import './${featureName}_view_model.dart';
+import './route/${featureName}_argument.dart';
+import './route/${featureName}_route.dart';
+import './screen/${featureName}_mobile_landscape.dart';
+import './screen/${featureName}_mobile_portrait.dart';
+
+class ${featureName.capitalize()}AdaptiveUi extends BaseAdaptiveUi<${featureName.capitalize()}Argument, ${featureName.capitalize()}Route> {
+  const ${featureName.capitalize()}AdaptiveUi({super.key});
+
+  @override
+  State<StatefulWidget> createState() => ${featureName.capitalize()}AdaptiveUiState();
+}
+
+class ${featureName.capitalize()}AdaptiveUiState extends BaseAdaptiveUiState<${featureName.capitalize()}Argument, ${featureName.capitalize()}Route, ${featureName.capitalize()}AdaptiveUi, ${featureName.capitalize()}ViewModel, ${featureName.capitalize()}Binding> {
+  @override
+  ${featureName.capitalize()}Binding binding = ${featureName.capitalize()}Binding();
+
+  @override
+  StatefulWidget mobilePortraitContents(BuildContext context) {
+    return ${featureName.capitalize()}MobilePortrait(viewModel: viewModel);
+  }
+
+  @override
+  StatefulWidget mobileLandscapeContents(BuildContext context) {
+    return ${featureName.capitalize()}MobileLandscape(viewModel: viewModel);
+  }
 }
 ''';
 
-String adaptiveUiTemplate(String featureName) => '''
-class ${featureName.capitalize()}AdaptiveUI {
-  // TODO: Add adaptive UI code here
-}
-''';
+String viewModelContent(String featureName) => '''
+import 'package:flutter/foundation.dart';
+import 'package:hello_flutter/presentation/base/base_viewmodel.dart';
+import './route/${featureName}_argument.dart';
+import './route/${featureName}_argument.dart';
+import './route/${featureName}_route.dart';
 
-String viewModelTemplate(String featureName) => '''
-class ${featureName.capitalize()}ViewModel {
-  // TODO: Add view model code here
+class ${featureName.capitalize()}ViewModel extends BaseViewModel<${featureName.capitalize()}Argument> {
+
+  final ValueNotifier<String> _message = ValueNotifier('$featureName');
+
+  ValueListenable<String> get message => _message;
+  
+  int count = 0;
+
+  ${featureName.capitalize()}ViewModel();
+
+  @override
+  void onViewReady({${featureName.capitalize()}Argument? argument}) {
+    super.onViewReady();
+  }
+  
+  void onClick() {
+     count++;
+    _message.value = '\${message.value}\$count';
+  }
+    
 }
 ''';
 
@@ -93,9 +220,9 @@ extension StringExtension on String {
     if (this == null) {
       throw ArgumentError("string: $this");
     }
-    if (this.isEmpty) {
+    if (isEmpty) {
       return this;
     }
-    return this[0].toUpperCase() + this.substring(1);
+    return this[0].toUpperCase() + substring(1);
   }
 }
