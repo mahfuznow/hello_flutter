55039818b7d7cc57765ad7ea6ca23b21a2485420
Md. Ohiduzzaman <150322633+OhiduzzamanOve@users.noreply.github.com>
2024-09-13 12:30:58 +0600

    Updated theme settings into separate widget

--- lib/presentation/feature/settings/screen/settings_mobile_portrait.dart
+++ lib/presentation/feature/settings/screen/settings_mobile_portrait.dart
@@ -3,6 +3,7 @@ import 'package:domain/model/app_theme_mode.dart';
 import 'package:flutter/material.dart';
 import 'package:hello_flutter/presentation/base/base_ui_state.dart';
 import 'package:hello_flutter/presentation/common/extension/context_ext.dart';
+import 'package:hello_flutter/presentation/feature/settings/screen/widgets/theme_settings.dart';
 import 'package:hello_flutter/presentation/feature/settings/settings_view_model.dart';
 
 class SettingsMobilePortrait extends StatefulWidget {
@@ -21,38 +22,7 @@ class SettingsMobilePortraitState extends BaseUiState<SettingsMobilePortrait> {
       body: SafeArea(
         child: Column(
           children: [
-            valueListenableBuilder(
-              listenable: widget.viewModel.appViewModel.selectedThemeMode,
-              builder: (context, selectedTheme) {
-                return Card(
-                  child: ExpansionTile(
-                    title: Text(
-                      context.localizations.theme,
-                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                            color: Theme.of(context).colorScheme.primary,
-                          ),
-                    ),
-                    children: [
-                      _buildThemeOption(
-                        AppThemeMode.system,
-                        context.localizations.system_default,
-                        selectedTheme,
-                      ),
-                      _buildThemeOption(
-                        AppThemeMode.light,
-                        context.localizations.light_theme,
-                        selectedTheme,
-                      ),
-                      _buildThemeOption(
-                        AppThemeMode.dark,
-                        context.localizations.dark_theme,
-                        selectedTheme,
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
+            ThemeSettings(viewModel: widget.viewModel),
             valueListenableBuilder(
               listenable: widget.viewModel.appViewModel.selectedLanguage,
               builder: (context, selectedLanguage) {
@@ -82,30 +52,6 @@ class SettingsMobilePortraitState extends BaseUiState<SettingsMobilePortrait> {
             ),
           ],
         ),
-      ),
-    );
-  }
-
-  Widget _buildThemeOption(
-    AppThemeMode theme,
-    String label,
-    AppThemeMode selectedTheme,
-  ) {
-    return ListTile(
-      title: Text(
-        label,
-        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-              color: Theme.of(context).colorScheme.primary,
-            ),
-      ),
-      trailing: Radio<AppThemeMode>(
-        value: theme,
-        groupValue: selectedTheme,
-        onChanged: (AppThemeMode? value) {
-          widget.viewModel.onThemeChanged(
-            value,
-          );
-        },
       ),
     );
   }
