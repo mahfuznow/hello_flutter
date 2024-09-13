420fcbf2b796a0b38c39dc98f15cbd2034cc1423
Md. Ohiduzzaman <150322633+OhiduzzamanOve@users.noreply.github.com>
2024-09-13 12:35:26 +0600

    Updated language settings into separate widget

--- lib/presentation/feature/settings/screen/settings_mobile_portrait.dart
+++ lib/presentation/feature/settings/screen/settings_mobile_portrait.dart
@@ -3,6 +3,7 @@ import 'package:domain/model/app_theme_mode.dart';
 import 'package:flutter/material.dart';
 import 'package:hello_flutter/presentation/base/base_ui_state.dart';
 import 'package:hello_flutter/presentation/common/extension/context_ext.dart';
+import 'package:hello_flutter/presentation/feature/settings/screen/widgets/language_settings.dart';
 import 'package:hello_flutter/presentation/feature/settings/screen/widgets/theme_settings.dart';
 import 'package:hello_flutter/presentation/feature/settings/settings_view_model.dart';
 
@@ -23,59 +24,9 @@ class SettingsMobilePortraitState extends BaseUiState<SettingsMobilePortrait> {
         child: Column(
           children: [
             ThemeSettings(viewModel: widget.viewModel),
-            valueListenableBuilder(
-              listenable: widget.viewModel.appViewModel.selectedLanguage,
-              builder: (context, selectedLanguage) {
-                return Card(
-                  child: ExpansionTile(
-                    title: Text(
-                      context.localizations.language,
-                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                            color: Theme.of(context).colorScheme.primary,
-                          ),
-                    ),
-                    children: [
-                      _buildLanguageOption(
-                        AppLanguage.en,
-                        context.localizations.en,
-                        selectedLanguage,
-                      ),
-                      _buildLanguageOption(
-                        AppLanguage.nb,
-                        context.localizations.nb,
-                        selectedLanguage,
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
+            LanguageSettings(viewModel: widget.viewModel),
           ],
         ),
-      ),
-    );
-  }
-
-  Widget _buildLanguageOption(
-    AppLanguage language,
-    String label,
-    AppLanguage selectedLanguage,
-  ) {
-    return ListTile(
-      title: Text(
-        label,
-        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-              color: Theme.of(context).colorScheme.primary,
-            ),
-      ),
-      trailing: Radio<AppLanguage>(
-        value: language,
-        groupValue: selectedLanguage,
-        onChanged: (AppLanguage? value) {
-          widget.viewModel.onLanguageChanged(
-            value,
-          );
-        },
       ),
     );
   }
